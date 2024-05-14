353b38483b6bb035ee1bff852a7fd95dc785c8d4
Amita Sahoo <amita.sahoo023@gmail.com>
2024-05-13 19:37:20 -0500

    requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.1",
+    author= "amita",
+    author_email="esite.asahoo@gmail.com",
+    packages=find_packages()
+
+)
