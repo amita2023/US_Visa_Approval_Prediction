e85781c2ee9c2d080436747e1edb43819bb144f1
Amita Sahoo <amita.sahoo023@gmail.com>
2024-06-08 19:48:39 -0500

    constant

--- demo.py
+++ demo.py
@@ -1,11 +1,5 @@
-from us_visa.logger import logging 
-from us_visa.exception import USvisaException
-import sys
+from us_visa.constants import *
 
-try:
-    a = 1/"10"
-except Exception as e:
-    logging.info(e)
-    raise USvisaException(e, sys) from e
 
+print(DATABASE_NAME)
 
